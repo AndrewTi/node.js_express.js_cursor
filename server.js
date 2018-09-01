193be95b4a5b1e91ed5e79c9d0b90d6ad4577247
Andrey Tyndyk <andreytyndyk@gmail.com>
2018-09-01 10:36:34 +0300

    file system

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
-const hello = require('./module_scope');
-require('./global');
+const fs = require('fs');
 
-console.log(hello, test);
+fs.readFile('./file.log', (err, file) => {
+    console.log(err, file.toString());
+})
+
+fs.appendFile('./append.log', 'Hello append', (err) => {
+    console.log(err)
+})
+
+fs.appendFile('./append2.log', 'Hello append', (err) => {
+    console.log(err)
+})
+
+fs.stat('./file.log', (err, info) => {
+    console.log(info);
+})
+
+fs.unlink('./append2.log', (err) => {
+    console.log(err);
+})
+
+fs.mkdir('./test2', (err, info) => {
+    console.log(err, info);
+})
+
+fs.rename('./append.log', './test/file2.log', (err, info) => {
+    console.log(err, info);
+})
+
+
+fs.readFileSync('./append3.log');
