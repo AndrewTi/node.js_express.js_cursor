983128dd3ac9e5603c2370ad6b431ce19ba29366
Andrey Tyndyk <andreytyndyk@gmail.com>
2018-09-01 11:44:07 +0300

    routes params

--- server.js
+++ server.js
@@ -4,14 +4,50 @@ const body = require('body-parser');
 const app = express();
 
 
-app.use(body.json())
-app.use((req, res) => {
-    console.log(req.query);
-    console.log(req.body.name);
+app
+    .use(body.json())
+    .param('id', (req, res, next, id) => {
 
-    // req.on('data', (file) => {
-    //     console.log(file.toString());
-    // })
-}).listen(3434, () => {
+        const user = query('SELECT * FROM users WHERE id =' + id, (err, user) => {
+
+            if(err)
+                return res.json('500');
+
+
+            if(!user)
+                return res.status(404);
+
+            req._user = user;
+            next();
+        });
+        
+
+        // bad code
+        req._user = 'test';
+        next();
+
+    })
+    .get('user/:id', (req, res) => {
+        req._user;
+    })
+    .post('user/:id')
+    .put('user/:id')
+    .delete('user/:id')
+    .get('/test/name/', (req, res) => {
+        console.log("GET:", req.query);
+    })
+    .post('/test', (req, res) => {
+        console.log("POST:", req.body);
+    })
+    .get('/test/:name/:id/', (req, res) => {
+        console.log(req.params, req.query, req._user);
+
+    })
+    .listen(3434, () => {
     console.log('PORT: 3434');
 })
+
+// user
+// user/:id
+
+// http://localhost:3434/test/dfhs5heh534/34234234
